133e9277e0d1fa688254b07bdefa4d8fa0020f19
xxwen12 <xiaoxiao.wen@ufl.edu>
2021-10-04 14:26:49 -0400

    1st R package

---
+++ R/g.R
@@ -0,0 +1,13 @@
+##' Subtract two numbers (Description)
+##'
+##' We want to Subtract two numbers, blalala... (Details)
+##' @title Subtract two numbers
+##' @param x first number
+##' @param y second number
+##' @return x - y
+##' @author xiaoxiao
+##' @export
+##' @examples
+##' g(2,1)
+##' 
+g <- function(x, y) x - y
